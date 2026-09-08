769bd737ea43a977a4d2f5e440beedd33626bae9
nairarama6 <nairarama6@gmail.com>
2026-09-08 22:06:13 +0700

    Tugas pekan 1

---
+++ pekan1/tugas1_2511533027.py
@@ -0,0 +1,19 @@
+# Program Tugas Pekan 1 - Profil & Kartu Nama Digital
+"""Program sederhana untuk menampilkan kartu nama digital
+Menggunakan print, komentar, string concatenation, dan escape character.
+pertemuan 1 praktikum alpro"""
+print("========================================")
+print("       KARTU NAMA DIGITAL MAHASISWA     ")
+print("========================================")
+print()
+print("Nama             : " + "Naira")
+print("NIM              : " + "2511533027")
+print("Program Studi    : " + "Informatika")
+print("Fakultas         : " + "Fakultas Teknologi Informasi")
+print("Kelas            : " + "Prak Alpro A")
+print()
+print("  /\\_/\\  ")
+print(" ( o.o ) ")
+print("  > ^ <  ")
+print("========================================")
+print("Program ini dibuat oleh:\nNaira - 2511533027")
